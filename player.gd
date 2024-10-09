19e83ea1e2f11f1711bf62f30faa29f2c9ddb6ee
Toby Moore <fireteebs@gmail.com>
2024-10-09 13:52:48 -0400

    added title sprite, working on hp funcs

--- player.gd
+++ player.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
-	
+
+var can_move
+
 var max_speed = 600
 var accel = 9999
 var friction = 9999
 var input = Vector2.ZERO
-var shitpant
+
+var hp = 100
+signal health_value
+signal dead
+
+func _on_control_start_game() -> void:
+	show()
+	can_move = true
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -16,7 +25,7 @@ func get_input():
 
 func player_movement(delta):
 	input = get_input()
-	if shitpant == true:
+	if can_move == true:
 		if input == Vector2.ZERO:
 			if velocity.length() > (friction * delta):
 				velocity -= velocity.normalized() * (friction * delta)
@@ -28,7 +37,12 @@ func player_movement(delta):
 	
 	move_and_slide()
 
-
-func _on_control_start_game() -> void:
-	show()
-	shitpant = true
+func hp_control():
+	if hp <= 0:
+		hp = 0
+		dead.emit()
+	else:
+		health_value.emit()
+	
+	
+	
