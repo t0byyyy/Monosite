d0b177ffa879be088152b8330a647f59d638db1a
t0byyyy <90530402+t0byyyy@users.noreply.github.com>
2024-10-23 15:22:11 -0400

    experiments with grav

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
 var can_move
+var min_jump_vel = -175
+var max_jump_vel = -400
+var jump_vel = -175
+var SPEED = 100
 
-var JUMP_VELOCITY = -800
-var SPEED = 400
+const gravity = 2000
+
+# jump logic
+var jumps_available = 2
+const total_jump_power = 1500
+const jump_decelleration = 3000
+var jump_power = total_jump_power
+
 
 var hp = 100
 signal health_value(hp)
@@ -16,14 +26,21 @@ func _on_control_start_game() -> void:
 func _physics_process(delta: float) -> void:
 	if can_move == true:
 		if not is_on_floor():
-			velocity += get_gravity() * delta
+			velocity.y += gravity * delta
 
 		# Handle jump.
-		if Input.is_action_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+		if jumps_available != 2 and is_on_floor():
+			jumps_available = 2
+			jump_power = total_jump_power
+			
+		if Input.is_action_pressed("ui_accept"):
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
+			velocity.y += -jump_power * delta
+			jump_power = jump_power - (jump_decelleration * delta)
+					
+		if Input.is_action_just_released("ui_accept"):
+			jumps_available -= 1
+
 		var direction := Input.get_axis("ui_left", "ui_right")
 
 		if direction:
