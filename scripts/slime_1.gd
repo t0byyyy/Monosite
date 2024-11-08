f52206b3235c258b179425a84b91564d3d35ede4
t0byyyy <90530402+t0byyyy@users.noreply.github.com>
2024-11-08 11:42:13 -0500

    more fresh, more clean
    slime comments

--- scripts/slime_1.gd
+++ scripts/slime_1.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+# physics vars
 var can_move = false
 var x_speed = 15
 var direction
@@ -7,9 +8,11 @@ const gravity = 200
 
 func _physics_process(delta: float) -> void:
 
+# grav
 	if !is_on_floor():
 		velocity.y += gravity * delta
 
+# follows player xpos
 	if player_xpos - position.x <= 100:
 		if can_move == true:
 			if player_xpos > position.x:
@@ -19,15 +22,18 @@ func _physics_process(delta: float) -> void:
 			move_and_slide()
 			velocity.x = x_speed*direction
 
+# receives jump countdown
 func _on_jump_timer_jump() -> void:
 	if can_move == true and is_on_floor():
 		velocity.y = -65
 
+# receives player xpos
 var player_xpos
 
 func _on_player_player_xpos(x: Variant) -> void:
 	player_xpos = x
 
+# receives game start
 func _on_control_start_game() -> void:
 	can_move = true
 	show()
