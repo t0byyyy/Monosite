4b5daba2aa6019cbd75f77b57ca6b821faaab292
t0byyyy <90530402+t0byyyy@users.noreply.github.com>
2024-11-14 14:30:21 -0500

    tweakin physics, fixed death
    capped fallspeed

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,27 +7,28 @@ func _on_control_start_game() -> void:
 	can_move = true
 
 # jump logic - needs variable jump
-const gravity = 300
+const gravity = 400
 var jump_power = 600
 var x_accel = 100
 var can_jump
 
 func _physics_process(delta: float) -> void:
-#	print(velocity.y)
+
 # gravity
 	if can_move == true:
-		if not is_on_floor():
+		if not is_on_floor() and velocity.y <= 600:
 			velocity.y += gravity * delta
 
 # handles jump, resets to base on floor
 		if is_on_floor() and not Input.is_action_pressed("ui_accept"):
 			can_jump = true
-		if velocity.y <= -125 or is_on_ceiling() or Input.is_action_just_released("ui_accept") or velocity.y >= 15:
+		if velocity.y <= -135 or is_on_ceiling() or Input.is_action_just_released("ui_accept") or velocity.y >= 15:
 			can_jump = false
 		if Input.is_action_pressed("ui_accept") and can_jump == true:
 			velocity.y += -(jump_power*delta)
 		if Input.is_action_just_pressed("ui_accept") and can_jump == true:
 			velocity.y += -85
+
 # handles directional inputs
 		var direction := Input.get_axis("ui_left", "ui_right")
 		
@@ -43,10 +44,9 @@ func _physics_process(delta: float) -> void:
 
 signal player_xpos(x)
 
-func _ready():
-	var parent = get_parent()
-	var slime = parent.get_node_or_null("slime_1/Area2D")
-	slime.touching_slime.connect(death)
+#handles death, remember to add Area2Ds with 2,2 collisions to interact with this
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	death()
 
 func death():
 	can_move = false
