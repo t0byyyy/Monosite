4192d5ef5f76bdafe9665238103946a9a07211b4
t0byyyy <90530402+t0byyyy@users.noreply.github.com>
2024-11-20 14:49:45 -0500

    spikes, dash, more ground

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,13 @@ extends Control
 
 signal start_game()
 
+#func _ready():
+	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+			get_tree().quit()
+
 func _on_button_pressed() -> void:
 	start_game.emit()
 	hide()
--- scripts/player.gd
+++ scripts/player.gd
@@ -11,6 +11,7 @@ const gravity = 400
 var jump_power = 600
 var x_accel = 100
 var can_jump
+var can_dash = true
 
 func _physics_process(delta: float) -> void:
 
@@ -20,28 +21,35 @@ func _physics_process(delta: float) -> void:
 			velocity.y += gravity * delta
 
 # handles jump, resets to base on floor
-		if is_on_floor() and not Input.is_action_pressed("ui_accept"):
+		if is_on_floor() and !Input.is_action_pressed("ui_accept"):
 			can_jump = true
 		if velocity.y <= -135 or is_on_ceiling() or Input.is_action_just_released("ui_accept") or velocity.y >= 15:
 			can_jump = false
 		if Input.is_action_pressed("ui_accept") and can_jump == true:
-			velocity.y += -(jump_power*delta)
+			velocity.y += -(jump_power * delta + abs(velocity.x) * delta)
 		if Input.is_action_just_pressed("ui_accept") and can_jump == true:
 			velocity.y += -95
 
 # dash
 		var direction := Input.get_axis("ui_left", "ui_right")
-		if Input.is_action_just_pressed("run") and direction:
-			velocity.x = 200 * direction
-
-# handles directional inputs
-		if direction and !Input.is_action_pressed("run"):
-			velocity.x = direction * x_accel
-		elif Input.is_action_pressed("run") and direction and abs(velocity.x) <= 700:
-			velocity.x += 300 * direction
+		print(velocity.x, " ", velocity.y, " ", can_dash)
+# handles directional inputs, dash
+		if direction:
+			if !Input.is_action_pressed("run") or can_dash == false:
+				velocity.x = direction * x_accel
+			if Input.is_action_pressed("run") and can_dash == true:
+				velocity.x += 150 * direction
+				velocity.y = 0
+			if Input.is_action_just_pressed("run") and can_dash == true:
+				velocity.x = 250 * direction
 		else:
 			velocity.x = move_toward(velocity.x, 0, x_accel)
-		print(velocity.x)
+		if abs(velocity.x) >= 1000 or Input.is_action_just_released("run"):
+			can_dash = false
+			can_jump = false
+		if is_on_floor() and !Input.is_action_pressed("run"):
+			can_dash = true
+
 		move_and_slide()
 
 # emits xpos for enemy tracking
